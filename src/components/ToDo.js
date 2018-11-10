87e196e4dad7a6e3adc16060a7ec99f87d997ca2
Adrienne King <AdrienneNKing@gmail.com>
2018-11-10 10:51:34 -0800

    React-to-do checkpoint state and props submission

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render () {
     return(
-      <li> A to-do will go here </li>
+      <li>{ this.props.description }</li>
     );
   }
 }
